f6d73158ffa6921253969e5883770f188005dfd1
zhongrongzhao <30423830+zhongrongzhao@users.noreply.github.com>
2020-04-18 21:15:26 +0800

    002鱼蛋的闷骚开发技巧

---
+++ 鱼蛋文件分割合并工具/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace 鱼蛋文件分割器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择需要分割/合并的文件";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "分割/合并文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "设置分割文件大小";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Byte",
+            "KB",
+            "MB",
+            "GB"});
+            this.comboBox1.Location = new System.Drawing.Point(140, 111);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "选择导出的路径";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(252, 21);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "导出位置";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 265);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(340, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(131, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 57);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "打开导出文件";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(25, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "分割文件";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(131, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "合并文件";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 294);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "鱼蛋文件分割合并工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 鱼蛋文件分割合并工具/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 鱼蛋文件分割器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static private string what_do_you_want_to_do = "";
+        /// <summary>
+        /// 选择文件
+        /// </summary>
+        private void SelectFile(string what)
+        {
+            if(what.Equals("splitfile"))
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();   //显示选择文件对话框 
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.textBox1.Text = openFileDialog1.FileName;     //显示文件路径 
+                }
+            }
+
+            if(what.Equals("combinfile"))
+            {
+                if (openFileDialog2.ShowDialog() == DialogResult.OK)
+                {
+                    string Selectfile = "";
+                    string[] files = openFileDialog2.FileNames;
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Selectfile += "," + files[i].ToString();
+                    }
+                    if (Selectfile.StartsWith(","))
+                    {
+                        Selectfile = Selectfile.Substring(1);
+                    }
+                    if (Selectfile.EndsWith(","))
+                    {
+                        Selectfile.Remove(Selectfile.LastIndexOf(","), 1);
+                    }
+                    this.textBox1.Text = Selectfile;
+                }
+
+            }
+        }
+        
+
+        /// <summary>
+        /// 选择文件夹 
+        /// </summary>
+        private void SelectPath(string what)
+        {
+            if (what.Equals("splitfile"))
+            {
+                if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (this.folderBrowserDialog1.SelectedPath.Trim() != "")
+                        this.textBox3.Text = this.folderBrowserDialog1.SelectedPath.Trim();
+                }
+            }
+            if (what.Equals("combinfile"))
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox3.Text = saveFileDialog1.FileName;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 分割文件
+        /// </summary>
+        /// <param name="strFlag">分割单位</param>
+        /// <param name="intFlag">分割大小</param>
+        /// <param name="strPath">分割后的文件存放路径</param>
+        /// <param name="strFile">要分割的文件</param>
+        /// <param name="PBar">进度条显示</param>
+        public void SplitFile(string strFlag, int intFlag, string strPath, string strFile, ProgressBar PBar)
+        {
+            int iFileSize = 0;
+            //根据选择来设定分割的小文件的大小
+            switch (strFlag)
+            {
+                case "Byte":
+                    iFileSize = intFlag;
+                    break;
+                case "KB":
+                    iFileSize = intFlag * 1024;
+                    break;
+                case "MB":
+                    iFileSize = intFlag * 1024 * 1024;
+                    break;
+                case "GB":
+                    iFileSize = intFlag * 1024 * 1024 * 1024;
+                    break;
+            }
+            //以文件的全路径对应的字符串和文件打开模式来初始化FileStream文件流实例
+            FileStream SplitFileStream = new FileStream(strFile, FileMode.Open);
+            //以FileStream文件流来初始化BinaryReader文件阅读器
+            BinaryReader SplitFileReader = new BinaryReader(SplitFileStream);
+            //每次分割读取的最大数据
+            byte[] TempBytes;
+            //小文件总数
+            int iFileCount = (int)(SplitFileStream.Length / iFileSize);
+            PBar.Maximum = iFileCount;
+            if (SplitFileStream.Length % iFileSize != 0) iFileCount++;
+            string[] TempExtra = strFile.Split('.');
+            //循环将大文件分割成多个小文件
+            for (int i = 1; i <= iFileCount; i++)
+            {
+                //确定小文件的文件名称
+                string sTempFileName = strPath + @"\" + i.ToString().PadLeft(4, '0') + "." + TempExtra[TempExtra.Length - 1]; //小文件名
+                //根据文件名称和文件打开模式来初始化FileStream文件流实例
+                FileStream TempStream = new FileStream(sTempFileName, FileMode.OpenOrCreate);
+                //以FileStream实例来创建、初始化BinaryWriter书写器实例
+                BinaryWriter TempWriter = new BinaryWriter(TempStream);
+                //从大文件中读取指定大小数据
+                TempBytes = SplitFileReader.ReadBytes(iFileSize);
+                //把此数据写入小文件
+                TempWriter.Write(TempBytes);
+                //关闭书写器，形成小文件
+                TempWriter.Close();
+                //关闭文件流
+                TempStream.Close();
+                PBar.Value = i - 1;
+            }
+            //关闭大文件阅读器
+            SplitFileReader.Close();
+            SplitFileStream.Close();
+            MessageBox.Show("文件分割成功!");
+        }
+
+        /// <summary>
+        /// 合并文件
+        /// </summary>
+        /// <param name="list">要合并的文件集合</param>
+        /// <param name="strPath">合并后的文件名称</param>
+        /// <param name="PBar">进度条显示</param>
+        public void CombinFile(string[] strFile, string strPath, ProgressBar PBar)
+        {
+            PBar.Maximum = strFile.Length;
+            FileStream AddStream = null;
+            //以合并后的文件名称和打开方式来创建、初始化FileStream文件流
+            AddStream = new FileStream(strPath, FileMode.Append);
+            //以FileStream文件流来初始化BinaryWriter书写器，此用以合并分割的文件
+            BinaryWriter AddWriter = new BinaryWriter(AddStream);
+            FileStream TempStream = null;
+            BinaryReader TempReader = null;
+            //循环合并小文件，并生成合并文件
+            for (int i = 0; i < strFile.Length; i++)
+            {
+                //以小文件所对应的文件名称和打开模式来初始化FileStream文件流，起读取分割作用
+                TempStream = new FileStream(strFile[i].ToString(), FileMode.Open);
+                TempReader = new BinaryReader(TempStream);
+                //读取分割文件中的数据，并生成合并后文件
+                AddWriter.Write(TempReader.ReadBytes((int)TempStream.Length));
+                //关闭BinaryReader文件阅读器
+                TempReader.Close();
+                //关闭FileStream文件流
+                TempStream.Close();
+                PBar.Value = i + 1;
+            }
+            //关闭BinaryWriter文件书写器
+            AddWriter.Close();
+            //关闭FileStream文件流
+            AddStream.Close();
+            MessageBox.Show("文件合并成功！");
+        }
+
+        /// <summary>
+        /// 选择文件按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(what_do_you_want_to_do == "")
+            {
+                MessageBox.Show("请选择”分割文件“或”合并文件“", "警告");
+            }
+            else
+            {
+                SelectFile(what_do_you_want_to_do);
+            }
+        }
+
+        /// <summary>
+        /// 选择文件按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (what_do_you_want_to_do == "")
+            {
+                MessageBox.Show("请选择”分割文件“或”合并文件“", "警告");
+            }
+            else
+            {
+                SelectPath(what_do_you_want_to_do);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 1;
+            timer1.Start();
+        }
+
+     
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(textBox1.Text != ""  && textBox3.Text != "")
+            {
+                button2.Enabled = true;
+                button4.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// 分割/合并文件按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (what_do_you_want_to_do.Equals("splitfile"))
+                    {
+                        if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+                        {
+                            MessageBox.Show("请填写好参数", "警告");
+                        }
+                        else
+                        {
+                            SplitFile(comboBox1.Text, Convert.ToInt32(textBox2.Text.Trim()), textBox3.Text, textBox1.Text, progressBar1);
+                        }
+                    }
+
+                if (what_do_you_want_to_do.Equals("combinfile"))
+                {
+                    if (textBox1.Text == "" || textBox3.Text == "")
+                    {
+                        MessageBox.Show("请填写好参数", "警告");
+                    }
+                    else
+                    {
+                        if (textBox1.Text.IndexOf(",") == -1)
+                        {
+                            MessageBox.Show("至少两个文件才能合成", "警告");
+                        }
+
+                        else
+                        {
+                            string[] text = textBox1.Text.Split(',');
+                            CombinFile(text, textBox3.Text, progressBar1);
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 打开导出文件按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string v_OpenFolderPath = textBox3.Text; System.Diagnostics.Process.Start("explorer.exe", v_OpenFolderPath);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox2.Enabled = true;
+            comboBox1.Enabled = true;
+            label2.Enabled = true;
+            what_do_you_want_to_do = "splitfile";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox2.Enabled = false;
+            comboBox1.Enabled = false;
+            label2.Enabled = false;
+            what_do_you_want_to_do = "combinfile";
+        }
+    }
+}
